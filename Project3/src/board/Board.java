3f25e8283e62d157f4d71fd1e859b92fc41b7b53
zparnold <zparnold@GL2-20.admin.wpi.edu>
2015-02-06 12:13:03 -0500

    initial commit

---
+++ Project3/src/board/Board.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package board;
+
+/**
+ * @author zparnold ssmaceachern
+ *
+ */
+public class Board {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
