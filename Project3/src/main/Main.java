cdd1851899b56c58463f808b7ac05f4793906090
zparnold <zparnold@GL2-19.admin.wpi.edu>
2015-02-07 10:15:49 -0500

    Adds command-line argument processing to Main.

--- Project3/src/main/Main.java
+++ Project3/src/main/Main.java
@@ -22,16 +22,15 @@ public class Main {
 	// Class vars
 	private static int BOARD_WIDTH = 7;
 	private static int BOARD_HEIGHT = 6;
+
 	/**
 	 * @param args
 	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException {
-				
-		System.out.println(Arrays.toString(args));
 
 		Main board = new Main();
-		board.readInFromCSV();
+		board.readInFromCSV(args[0], args[1]);
 
 	}
 
@@ -39,13 +38,16 @@ public class Main {
 	 * reads in the training data from the CSV file and spawns off new board
 	 * classes to be evaluated
 	 * 
+	 * @param outPath
+	 * @param inPath
+	 * 
 	 * @throws IOException
 	 */
-	private void readInFromCSV() throws IOException {
+	private void readInFromCSV(String inPath, String outPath)
+			throws IOException {
 
-		String myPath = new File("src/files/trainDataSet.csv")
-				.getAbsolutePath();
-		String outPath = new File("src/files/out.csv").getAbsolutePath();
+		String myPath = new File(inPath).getAbsolutePath();
+		String outFilePath = new File(outPath).getAbsolutePath();
 		String csvFile = myPath;
 		BufferedReader br = null;
 		String line = "";
@@ -54,43 +56,44 @@ public class Main {
 		try {
 
 			br = new BufferedReader(new FileReader(csvFile));
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outPath));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(
+					outFilePath));
 			while ((line = br.readLine()) != null) {
-				if (count > 5 && count < 10){
-				int[][] stringBoard = new int[BOARD_HEIGHT][BOARD_WIDTH];
-				String[] holder = line.split(csvSplitBy);
-				int whoWon = Integer.parseInt(holder[holder.length - 1]);
+				if (count > 5 && count < 10) {
+					int[][] stringBoard = new int[BOARD_HEIGHT][BOARD_WIDTH];
+					String[] holder = line.split(csvSplitBy);
+					int whoWon = Integer.parseInt(holder[holder.length - 1]);
 
-				// Read in the file here (probably a for loop that's been
-				// unrolled in 1 direction)
-				// Don't forget that the last column is an eval of who won.
-				for (int i = 0; i < 6; i++) {
-					for (int j = 0; j < 7; j++) {
-						stringBoard[i][j] = Integer.parseInt(holder[(i*BOARD_WIDTH) + j]);
+					// Read in the file here (probably a for loop that's been
+					// unrolled in 1 direction)
+					// Don't forget that the last column is an eval of who won.
+					for (int i = 0; i < 6; i++) {
+						for (int j = 0; j < 7; j++) {
+							stringBoard[i][j] = Integer
+									.parseInt(holder[(i * BOARD_WIDTH) + j]);
+						}
 					}
-				}
-				
-				
-				Board newBoard = new Board(stringBoard, whoWon);
-				
-				System.out.println("Board:");
-				for (int[] s : stringBoard){
-					for (int i : s){
-						System.out.print(i + ",");
-					}
-					System.out.println();
-				}
-				
-				newBoard.evaluate();
-				//System.out.println("Line: "+line);
-				//System.out.println(Arrays.deepToString(stringBoard));
-				
-				//printBoard(line);
-				// write now back to the file
 
-				writer.write(line);
-				writer.newLine();
-				// writer.write(newBoard.getEvaluation().toString());
+					Board newBoard = new Board(stringBoard, whoWon);
+
+					System.out.println("Board:");
+					for (int[] s : stringBoard) {
+						for (int i : s) {
+							System.out.print(i + ",");
+						}
+						System.out.println();
+					}
+
+					newBoard.evaluate();
+					// System.out.println("Line: "+line);
+					// System.out.println(Arrays.deepToString(stringBoard));
+
+					// printBoard(line);
+					// write now back to the file
+
+					writer.write(line);
+					writer.newLine();
+					// writer.write(newBoard.getEvaluation().toString());
 				}
 				count++;
 			}
@@ -113,11 +116,11 @@ public class Main {
 
 		System.out.println("Done");
 	}
-	
-	private void printBoard(String board){
+
+	private void printBoard(String board) {
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 7; j++) {
-				System.out.print(board.charAt((i*7)+j) + " ");
+				System.out.print(board.charAt((i * 7) + j) + " ");
 			}
 			System.out.print("\n");
 		}
